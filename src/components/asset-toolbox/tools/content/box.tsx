953bffefdce3908210127bf8f475ab87a0406abb
Ryan O'Connor <emailryanoconnor@gmail.com>
2020-12-19 21:23:10 +1300

    test ids

--- src/components/asset-toolbox/tools/content/box.tsx
+++ src/components/asset-toolbox/tools/content/box.tsx
@@ -1,18 +1,31 @@
 import { fabric } from "fabric";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import CanvasContext from "../../../canvas-context";
 
+export interface CraftarRectOptions extends fabric.IRectOptions {
+  id: string,
+  craftarType: string
+
+}
 export default function Box() {
-  const { addObject } = useContext(CanvasContext);
+  const id = 'box';
+  const { addObject, objects } = useContext(CanvasContext);
   const box = new fabric.Rect({
-    width: 50,
-    height: 50,
-    stroke: 'blue',
-    fill: "white",
-    strokeWidth: 3,
-    top: 20,
-    left: 20
+      width: 50,
+      height: 50,
+      stroke: 'blue',
+      fill: 'white',
+      strokeWidth: 3,
+      top: 20,
+      left: 20,
+      id: objects.filter((o: any)=> o.craftarType === id).length++,
+      craftarType: id,
+    } as unknown as CraftarRectOptions);
+
+    box.on('mousedown', function() { 
+      console.log('Im a box')
   });
+
   return (
 
     <button onClick= {() => { addObject(box)}}>
