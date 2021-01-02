f31dce2f3101843cc1054b56f833737ce40a3772
Ryan O'Connor <emailryanoconnor@gmail.com>
2021-01-02 23:16:15 +1300

    set active object + mouse up

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -13,9 +13,20 @@ export default function Canvas() {
     canvas.setHeight(900);
     canvas.setWidth(700);
 
-    objects.forEach((object: fabric.Object) => {
-      canvas.add(object)
+    objects.forEach((object: fabric.Object, key: number) => {
+      canvas.add(object);
+      if (Object.is(objects.length - 1, key)) {
+        canvas.setActiveObject(object);
+      }
+        
     });
+
+
+    canvas.on('mouse:up', (e) => {
+      if (e.target) {
+        console.log(e.target)
+      }
+    })
     
     // UseEffect's cleanup function
     return () => {
